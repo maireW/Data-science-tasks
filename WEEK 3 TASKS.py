e8ec8b129d540220df07e4d4c5856115ce604975
maireW <76253416+maireW@users.noreply.github.com>
2021-01-25 16:44:41 +0000

    Add files via upload

---
+++ WEEK 3 TASKS.py
@@ -0,0 +1,26 @@
+#WEEK 3 TASK 1A Write a program that allows you to enter 4 numbers and stores them in a file called “Numbers”. Have a go at ‘w’ ‘r’ ‘a’
+my_file = open("Numbers.txt","w")
+my_1d_list = [3,45,83,21]
+print (my_1d_list)
+my_file.close()
+
+my_file = open("Numbers.txt", "a")
+my_1d2_list = [1, 3, 5, 7]
+print (my_1d2_list)
+my_file.close()
+
+#task 2 Write a program to ask a student for their percentage mark and convert this to a grade
+def mark_grade(inp_result):
+    result =  inp_result
+    if result  > 80:
+        print ("You got an A*")
+    elif result >70:
+        print ("You got a A")
+    elif result >60:
+        print ("You got a B")
+    elif result >50:
+        print ("You got a C")
+    elif result <50:
+        print ("You did not pass the exam")
+result =int( input ("what is your percentage mark?"))
+mark_grade(result)
