75da95e7c863bdc9407bba8ce0ebf982bbe3e63e
maireW <76253416+maireW@users.noreply.github.com>
2021-01-26 02:38:45 +0000

    Update np task week 3.py

--- np task week 3.py
+++ np task week 3.py
@@ -13,7 +13,7 @@ array3 = np.arange(1,11,2)
 print(array3)
 
 #task 5 convert 1d array to 2d with 2 rows)
-array_5 = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+array_5 = np.array([1,2,3,4,5,6,7,8,9,10])
 print(array_5)
 newArray_5 = array.reshape(2,5)
 print(newArray_5)
